ea61809d242e999b60d6b4c257d6ced914a10c07
shadofficialpro <68260334+shadofficialpro@users.noreply.github.com>
2025-10-04 18:29:28 -0400

    questions

---
+++ code/courses/quizzes/34220-exam-1/quiz-data.js
@@ -0,0 +1,37 @@
+const questionBank = [
+  {
+    "question": "The SNTP protocol consistently checks time and uses a drift file to ensure accuracy.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "An NTP stratum 2 or greater host must have at least two time sources to have reliable time information.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "In a Virtual Machine environment, you should synchronize each VM to its NTP synched host.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "The purpose of Marzullo's algorithm is to take into account the transmission duration of the time information in NTP.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "Proper functioning of a Microsoft Active Directory requires clients and servers to agree on the time within five minutes.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  }
+];
