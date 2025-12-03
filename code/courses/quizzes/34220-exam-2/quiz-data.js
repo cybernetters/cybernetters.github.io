b68322a50d30d32067302f9e077cf0b1e9892ed1
shadofficialpro <68260334+shadofficialpro@users.noreply.github.com>
2025-12-02 20:05:37 -0500

    questio

---
+++ code/courses/quizzes/34220-exam-2/quiz-data.js
@@ -0,0 +1,172 @@
+const questionBank = [
+  {
+    "question": "A transparent proxy server must spoof the source address of packets sent from the requesting client to the actual web server.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "When providing web services, it is best to configure www.domain.name as an A record rather than a CNAME record.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "When implementing a reverse proxy server, the destination web server doesn't know the address of the requesting client.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "A transparent proxy server must spoof the source address of packets sent from the web server to the actual requesting client.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "When providing web services, it is best to configure www.domain.name as a CNAME record rather than an A record.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "When implementing a reverse proxy server, the destination web server doesn't know the address of the requesting client.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "The single point of failure created by an application delivery controller makes their use limited, as there is no way to mitigate this risk.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "In full proxy load balancing, the load balancer becomes the server from the client perspective.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "When load balancing web applications, each TCP connection can be sent to a different server with impunity.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "When load balancing DNS servers, each request can be sent to a different server with impunity.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "Application delivery controllers always provide some level of load balancing functionality.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "Public key encryption is used for the majority of the data transmitted in HTTPS.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "When using X.509, a certificate is trusted based on the trust hierarchy of its issuing certificate authority.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "In a standard DNSSEC implementation, the root name servers are automatically trusted.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "DNSSEC Chains of Trust must begin at the root name servers.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "A TLS connection is opened before the initial BIND operation when using LDAPS.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "Kerberos only offers authentication functionality.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "Provides verification for DNS answers",
+    "answers": [
+      { "text": "DS", "correct": false },
+      { "text": "DNSKEY", "correct": false },
+      { "text": "RRSIG", "correct": true },
+      { "text": "NSEC", "correct": false },
+      { "text": "CDS", "correct": false }
+    ]
+  },
+  {
+    "question": "Used to prove a requested record doesn't exist",
+    "answers": [
+      { "text": "DS", "correct": false },
+      { "text": "DNSKEY", "correct": false },
+      { "text": "RRSIG", "correct": false },
+      { "text": "NSEC", "correct": true },
+      { "text": "CDS", "correct": false }
+    ]
+  },
+  {
+    "question": "Used for child-parent signaling",
+    "answers": [
+      { "text": "DS", "correct": false },
+      { "text": "DNSKEY", "correct": false },
+      { "text": "RRSIG", "correct": false },
+      { "text": "NSEC", "correct": false },
+      { "text": "CDS", "correct": true }
+    ]
+  },
+  {
+    "question": "Used to store public keys",
+    "answers": [
+      { "text": "DS", "correct": false },
+      { "text": "DNSKEY", "correct": true },
+      { "text": "RRSIG", "correct": false },
+      { "text": "NSEC", "correct": false },
+      { "text": "CDS", "correct": false }
+    ]
+  },
+  {
+    "question": "Information about a child zone published by the parent zone",
+    "answers": [
+      { "text": "DS", "correct": true },
+      { "text": "DNSKEY", "correct": false },
+      { "text": "RRSIG", "correct": false },
+      { "text": "NSEC", "correct": false },
+      { "text": "CDS", "correct": false }
+    ]
+  }
+
+];
