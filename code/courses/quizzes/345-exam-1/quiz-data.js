d7ad55db718255abf47a11db14f0e9d281ee5828
shadofficialpro <68260334+shadofficialpro@users.noreply.github.com>
2025-12-16 23:35:46 -0500

    345

---
+++ code/courses/quizzes/345-exam-1/quiz-data.js
@@ -0,0 +1,144 @@
+const questionBank = [
+  {
+    "question": "A switch segments a MAC broadcast domain when one VLAN is present.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "All inter-VLAN traffic must be routed.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "There are 4096 unique VLANs possible using IEEE 802.1Q.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "The Spanning Tree Protocol is used to break switching or bridging loops.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "The STP root switch election process is based solely on MAC address of the participating ports.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "An aggregate link has multiple interfaces from one physical switch logically bound together to act as one.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "You can configure multiple VLANs on a single Spanning Tree Instance using PVST.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "With the introduction of gigabit Ethernet networks, there is no longer a need for aggregate links.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "You can configure multiple VLANs on a single Spanning Tree Instance using MSTP.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "Layer 3 redundancy allows for more than one router to host the gateway.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "Networks should be designed based only on the amount of traffic they currently handle, not any future expectations.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "For a trunk port on a switch, the egress traffic is tagged and the ingress traffic is expected to have a tag.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "CIDR stands for Class-based Inter Domain-based Routing and relies on the notation with the “/”.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "VTP should always be used on layer 2 devices to provide the best security and network understanding.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "The penultimate subnet is the second to last subnet.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "A subnet mask and IP address use a logical OR operator to determine the network ID.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "RIPv2 requires IPv6 to be used.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "When supernetworking a set of network segments, you should ensure that all segments are available for use before assigning the resulting network ID.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "Parallel Backbone is the simplest backbone configuration.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "An Aggregate Backbone adds redundancy, fault tolerance, and potential address security.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  }
+
+
+  ];
