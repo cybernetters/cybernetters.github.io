0433f8ba3f33efb4b4cafc8c4aec5167e4fe73b7
shadofficialpro <68260334+shadofficialpro@users.noreply.github.com>
2026-03-10 17:02:10 -0400

    315

---
+++ code/courses/quizzes/315-exam-1/quiz-data.js
@@ -0,0 +1,143 @@
+const questionBank = [
+  {
+    "question": "Recursion uses a loop?",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "A C function must have a return value?",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "A C function uses a \"prototype\"?",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "The printf function is used to read input from the user in C.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "A variable declared inside a function has global scope by default.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "int *p; means that p is a pointer to an integer.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "The entry point, from the operating system, always starts with the main function.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "The format specifier %c in scanf() can be used to read a string.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "The if statement must always be followed by an else statement.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "The condition inside an if statement must be enclosed in parentheses ().",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "If the condition of a while loop is always true, it results in an infinite loop.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "The return 0; statement inside the main function indicates a successful program termination.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "When using scanf() to read an integer, you must use the & operator before the variable name.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "The expression array[i] is equivalent to *(array + i).",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "Where you have the following declarations: int *ptrToFun; and int var = 0;, the assignment ptrToFun = &var; is valid.",
+    "answers": [
+      { "text": "True", "correct": true },
+      { "text": "False", "correct": false }
+    ]
+  },
+  {
+    "question": "The following declaration is valid: char *message = 1993;",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "ANSI C allows the programmer to access any addressable memory space on the computer.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "Static variables are set at runtime.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "ANSI C is an excellent language for performing high level tasks on the internet.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  },
+  {
+    "question": "In a Towers of Hanoi game, the optimal number of moves to solve a 5 disk puzzle is 63.",
+    "answers": [
+      { "text": "True", "correct": false },
+      { "text": "False", "correct": true }
+    ]
+  }
+
+];
